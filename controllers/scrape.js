616e4c4be11c604f01356c7662f7217756a40f27
overcast92 <128191779+knoxgraeme@users.noreply.github.com>
2024-08-29 20:36:33 -0700

    Update scrape.js

--- controllers/scrape.js
+++ controllers/scrape.js
@@ -3,10 +3,11 @@ const axios = require('axios');
 async function getSource(search) {
     console.log(`Fetching source for search: ${JSON.stringify(search)}`);
     try {
+        const url = `https://www.facebook.com/marketplace/${search.city}/search/?query=${encodeURIComponent(search.query)}${search.maxPrice ? '&maxPrice=' + search.maxPrice : ''}`;
         const response = await axios.get('https://app.scrapingbee.com/api/v1', {
             params: {
                 'api_key': process.env.scrapingbee_api_key,
-                'url': `https://www.facebook.com/marketplace/${search.location}/search?daysSinceListed=1&query=${search.term.replace(/ /g,'%20')}&exact=false`,
+                'url': url,
                 'render_js': 'true',  // Ensure JS is rendered
                 'premium_proxy': 'true' // Keep proxy settings if needed
             }
