7f855b9291bba202ecd402d6509d7ece96f6f85e
overcast92 <128191779+knoxgraeme@users.noreply.github.com>
2024-08-29 19:32:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -73,12 +73,12 @@ app.get('/api/items', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/src')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/public/index.html'));
 });
 
 // Schedule the scraping task
