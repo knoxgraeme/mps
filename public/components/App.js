2d7abfe2bd28c25852a29997f01ee52df0f19f69
overcast92 <128191779+knoxgraeme@users.noreply.github.com>
2024-08-30 02:01:23 -0700

    Update App.js

--- public/components/App.js
+++ public/components/App.js
@@ -45,16 +45,6 @@ const Auth = () => {
     );
 };
 
-const App = () => {
-    return React.createElement('div', { className: "bg-gray-100 min-h-screen" },
-        React.createElement('header', { className: "bg-blue-600 text-white p-4 shadow-md" },
-            React.createElement('div', { className: "container mx-auto flex justify-between items-center" },
-                React.createElement('h1', { className: "text-2xl font-bold" }, "Facebook Marketplace Scraper"),
-                React.createElement(Auth)
-            )
-        ),
-        React.createElement('main', { className: "mt-6" },
-            React.createElement('p', null, "Content goes here")
-        )
-    );
-};
+// This file is no longer needed as we've moved to vanilla JavaScript.
+// The functionality has been integrated into public/js/app.js
+// You can safely delete this file.
